f301d9d1204280511cee4e53ee01f0388aa823c7
burcu <koral.burcu@hotmail.com>
2019-06-18 13:52:52 +0300

    first version

---
+++ Veresiye.Model/Activity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veresiye.Model
+{
+    public class Activity:BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public ActivityType AcivityType { get; set; }
+        public int CompanyId { get; set; }
+        public virtual Company Company { get; set; }
+
+
+
+    }
+}
